b00bc18eb0e6e204e2dd73a76de23c1d2371442a
franck <franck.hubschwerle@uha.fr>
2020-02-14 21:01:20 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color,
+                                 SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
+
+# Write your program here
+#brick.sound.beep()
+
+###########################################
+ # Main method
+
+ #motors
+ mRight = Motor(Port.D)
+ mLeft = Motor(Port.A)
+
+ #sensor
+ sRight = ColorSensor(Port.S4)
+
+ 
+
