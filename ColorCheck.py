a2ba45b6eaaf744e4031c78acc4f66fe3956c584
franck <franck.hubschwerle@uha.fr>
2020-02-16 01:34:32 +0100

    l'idée général

---
+++ ColorCheck.py
@@ -0,0 +1,13 @@
+#Récupérer les valeurs de la ligne noire à plusieurs endroits dans une séries de 4-5 prélèvment. 
+#Récupérer les valeurs de certains verts
+#faire une moyenne de chaque type de valeur et retourner cette valeur. 
+#___________________________________________________________________________________________________#
+
+#s'en servir pour valeur de comparaison:
+#exemple:
+#if colorSensor.color() < laMoyenne
+#   do something
+
+#___________________________________________________________________________________________________#
+
+
